a17417d9bcd79e167ecd70a6285030fca138f1ef
raul <192793@iberopuebla.mx>
2023-11-29 09:38:36 -0600

    ya esta lexer y parser, falta hacer bonito parser

--- pars.py
+++ pars.py
@@ -1,77 +1,38 @@
 import sys
 import json
-class Parser:
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.current_token = None
-        self.index = -1
-        self.next_token()
-    
-    def next_token(self):
-        self.index += 1
-        if self.index < len(self.tokens):
-            self.current_token = self.tokens[self.index]
+import pickle
+class Node:
+    def __init__(self, type, value=None, children=None):
+        self.type = type
+        self.value = value
+        self.children = children if children is not None else []
+
+    def __repr__(self):
+        return f"{self.type}: {self.value} -> {self.children}"
+
+
+def parse(tokens):
+    def parse_expression(index):
+        token_type, token_value = tokens[index]
+        if token_type == 'IPAREN':
+            return parse_list(index)
+        elif token_type in ['NUMERO', 'IDENTIFIER', 'STRING', 'RESERVED']:
+            return Node(token_type, token_value), index + 1
         else:
-            self.current_token = ('EOF', 'EOF')
-    
-    def parse(self):
-        if self.current_token[0] == 'EOF':
-            return None
-        else:
-            return self.parse_expr()
-    
-    def parse_expr(self):
-        if self.current_token[0] == 'IPAREN':
-            return self.parse_list()
-        elif self.current_token[0] == 'NUMERO':
-            return self.parse_number()
-        elif self.current_token[0] == 'IDENTIFIER':
-            return self.parse_identifier()
-        elif self.current_token[0] == 'STRING':
-            return self.parse_string()
-        elif self.current_token[0] == 'RESERVED':
-            return self.parse_reserved()
-        else:
-            raise SyntaxError(f"Unexpected token: {self.current_token[1]}")
-    
-    def parse_reserved(self):
-        op = self.current_token[1]
-        self.next_token()  # consume 'IDENTIFICADOR'
-        return op
-    
-    def parse_list(self):
-        elements = []
-        self.next_token()  # consume 'IPAREN'
-        while self.current_token[0] != 'DPAREN':
-            elements.append(self.parse_expr())
-        self.next_token()  # consume 'DPAREN'
-        return elements
-    
-    def parse_number(self):
-        value = self.current_token[1]
-        self.next_token()  # consume 'NUMERO'
-        return float(value) #if '.' in value else int(value)
-    
-    def parse_identifier(self):
-        name = self.current_token[1]
-        self.next_token()  # consume 'IDENTIFICADOR'
-        return name
-    
-    def parse_string(self):
-        string_value = self.current_token[1]
-        self.next_token()  # consume 'CADENA'
-        return string_value
-    
-def tree_print(node, level=0):
-    indent = '  ' * level
-    if isinstance(node, list):
-        print(indent + '[')
-        for child in node:
-            tree_print(child, level + 1)
-        print(indent + ']')
-    else:
-        print(indent + str(node))
-        
+            raise ValueError(f"Unexpected token: {token_type}")
+
+    def parse_list(index):
+        children = []
+        index += 1  # Skip the opening parenthesis
+        while tokens[index][0] != 'DPAREN':
+            node, index = parse_expression(index)
+            children.append(node)
+        return Node('LIST', children=children), index + 1
+
+    ast, _ = parse_expression(0)
+    return ast
+
+
     
 if __name__=='__main__':
     #name = sys.argv[1:]
@@ -81,12 +42,15 @@ if __name__=='__main__':
         lines = json.load(file)
         
     #print(lines)
-    parser = Parser(lines)
+    # Parse the tokens
+    ast = parse(lines)
+    print(ast)
+    
 
     # Parse the tokens to build a syntax tree
-    syntax_tree = parser.parse()
-    with open ('syn_tree.json',"w") as file:
-        json.dump(syntax_tree, file)
-    print(syntax_tree)
-    tree_print(syntax_tree)
+    # syntax_tree = parser.parse()
+    with open ('syn_tree.pickle',"wb") as file:
+        pickle.dump(ast, file)
+    #print(syntax_tree)
+    #tree_print(syntax_tree)
         
